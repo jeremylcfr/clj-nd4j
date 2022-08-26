94c7504d55ed789bfa6c0e1fa5c43653e2654e49
JeremyLC <jeremylecorguille@live.fr>
2022-08-26 23:46:51 +0200

    test

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [org.nd4j/nd4j-api "1.0.0-M2.1"]
                  [org.nd4j/nd4j-common "1.0.0-M2.1"]
                  [org.nd4j/nd4j-native "1.0.0-M2.1"]] 
-  :repositories [["github" {:url "https://maven.pkg.github.com/"
+  :repositories [["github" {:url "https://maven.pkg.github.com/jeremylcfr/clj-java-commons"
                             :username "jeremylcfr" 
                             :password :env/GITHUB_TOKEN 
                             :sign-releases false}]]
