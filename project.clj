2715e747a71d96cea921a5c29f252abb508ccdc0
JeremyLC <jeremylecorguille@live.fr>
2022-08-26 23:57:13 +0200

    oups, bad version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/algo.generic "0.1.3"]
-                 [jeremylcfr/clj-java-commons "0.1.0-SNAPSHOT"]
+                 [jeremylcfr/clj-java-commons "0.1.0"]
                  [org.nd4j/nd4j-api "1.0.0-M2.1"]
                  [org.nd4j/nd4j-common "1.0.0-M2.1"]
                  [org.nd4j/nd4j-native "1.0.0-M2.1"]] 
