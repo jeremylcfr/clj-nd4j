e737059a90f2a3f43d1c6a17dda5efd5476ae146
JeremyLC <jeremylecorguille@live.fr>
2022-08-27 00:52:32 +0200

    new group id

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject jeremylcfr/clj-nd4j "0.1.0-SNAPSHOT"
+(defproject io.github.jeremylcfr/clj-nd4j "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Apache License"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/algo.generic "0.1.3"]
-                 [jeremylcfr/clj-java-commons "0.1.0"]
+                 [io.github.jeremylcfr/clj-java-commons "1.0.0"]
                  [org.nd4j/nd4j-api "1.0.0-M2.1"]
                  [org.nd4j/nd4j-common "1.0.0-M2.1"]
                  [org.nd4j/nd4j-native "1.0.0-M2.1"]] 
