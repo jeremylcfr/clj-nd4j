5994220f76df275a43abdf1792c9b7f46aa87ff6
JeremyLC <jeremylecorguille@live.fr>
2022-08-26 23:59:40 +0200

    correct test

--- test/clj_nd4j/core_test.clj
+++
@@ -1,3 +0,0 @@
-(ns clj-nd4j.core-test
-  (:require [clojure.test :refer :all]
-            [clj-nd4j.core :refer :all]))
---
+++ test/clj_nd4j/ndarray_test.clj
@@ -0,0 +1,3 @@
+(ns clj-nd4j.ndarray-test
+  (:require [clojure.test :refer :all]
+            [clj-nd4j.ndarray :refer :all]))
