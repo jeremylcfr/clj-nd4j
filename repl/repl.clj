4ef840c2295fe7c30b6c1886a79697a8bc74cc8c
JeremyLC <jeremylecorguille@live.fr>
2022-11-27 14:57:46 +0100

    updaters

--- repl/repl.clj
+++ repl/repl.clj
@@ -1,6 +1,10 @@
 (ns repl
   (:require [clj-nd4j [ndarray :as nda]
                       [dataset :as ndd]]
+            [clj-nd4j.ml [activation :as mla]
+                         [gradient :as mlg]
+                         [loss :as mll]
+                         [updaters :as mlu]]
             [clj-java-commons [core :refer :all]
                               [coerce :refer [->clj]]])
   (:import [org.nd4j.linalg.factory Nd4j])
